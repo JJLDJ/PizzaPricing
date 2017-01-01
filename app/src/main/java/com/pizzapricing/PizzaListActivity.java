8a84d5f5298ed24dbf2a96c9adfb87a909d87bff
Jonathan <jjldj@jjldj.com>
2017-01-01 15:44:17 -0800

    Started trying to add support for recyler view again. Existing mechanism for adding text views to list activity no longer works. Maybe the recycler view takes over.

--- app/src/main/java/com/pizzapricing/PizzaListActivity.java
+++ app/src/main/java/com/pizzapricing/PizzaListActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,10 +16,27 @@ public class PizzaListActivity extends AppCompatActivity {
 
     public static final int ADD_PIZZA_INTENT_ID = 1;
 
+    private RecyclerView mRecyclerView;
+    private PizzaListAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza_list);
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.pizza_list_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        // specify an adapter (see also next example)
+        mAdapter = new PizzaListAdapter();
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     /** Called when the user wants to add a new pizza. */
